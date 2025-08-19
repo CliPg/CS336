c8af4f8b1430a1f821d84f867e6d6c35aff904b0
CliPg <775075825@qq.com>
2025-08-19 20:33:47 +0800

    add a5

---
+++ assignment5-alignment/cs336_alignment/math_baseline.py
@@ -0,0 +1,9 @@
+from vllm import LLM, SamplingParams
+
+def evaluate_vllm(
+    vllm_model: LLM,
+    reward_fn: Callable[[str,str], dict[str, float]],
+    prompts: List[str],
+    eval_sampling_params: SamplingParams
+) -> None:
+
